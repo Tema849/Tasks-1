dc6f7a1f6cde4d60b35fd44f38e461fd5e30ee9c
Artem <qwaqwatern@gmail.com>
2023-03-11 23:30:48 +0300

    Task 4

--- Program.cs
+++ Program.cs
@@ -13,3 +13,25 @@
 // {
 //     Console.WriteLine($"Большее число: {b}, меньшее число: {a}");
 // }    
+
+
+
+
+
+
+
+ // Напишите программу, которая принимает на вход три
+ // числа и выдаёт максимальное из этих чисел.
+ 
+ // Task 4
+
+// Console.WriteLine("Введите первое число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+// Console.WriteLine("Максимальное из этих чисел = " + max);
