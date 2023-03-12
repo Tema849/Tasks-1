58039a7842fcd5318edd9efc7f5ed96d272a2bd7
Artem <qwaqwatern@gmail.com>
2023-03-12 10:04:56 +0300

    Task 8

--- Program.cs
+++ Program.cs
@@ -54,3 +54,24 @@
 //     Console.WriteLine($"Число: {a} чётное.");
 // else
 //     Console.WriteLine($"Число: {a} нечётное.");  
+
+
+
+
+
+
+
+
+// Задача 8
+// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// Task 8
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+// {
+// if (i % 2 == 0)
+//     Console.Write(i+ ", ");
+// }    
